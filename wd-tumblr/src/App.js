c86be420da0b0b538cd9335b839f2ab0e893bbf1
chany <lyhy0310@gmail.com>
2018-12-13 01:03:51 +0900

    add (dev:setting): reactRouter 기본 Page 몇 개 Rendering

--- wd-tumblr/src/App.js
+++ wd-tumblr/src/App.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import DashBoard from './DashBoard';
+import Explore from './Explore';
+import InBox from './InBox';
+
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Links = () => (
+  // eslint-disable-next-line no-unused-expressions
+  <nav className="links">
+    <NavLink to="/">DashBoard</NavLink>
+    <NavLink to="/explore">Explore</NavLink>
+    <NavLink to="/InBox">InBox</NavLink>
+  </nav>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Links />
+      <Switch>
+        <Route exact path="/" component={DashBoard} />
+        <Route path="/explore" component={Explore} />
+        <Route path="/inBox" component={InBox} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ wd-tumblr/src/InBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InBox = props => {
+  return <div>InBox </div>;
+};
+
+export default InBox;
