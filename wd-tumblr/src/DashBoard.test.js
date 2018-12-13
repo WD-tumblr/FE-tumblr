1b9423971de744568586ce2b40c4218b6b4d491c
chany <lyhy0310@gmail.com>
2018-12-13 15:16:23 +0900

    add (dev:setting test): Header shallow test example add

--- wd-tumblr/src/DashBoard.test.js
+++ wd-tumblr/src/DashBoard.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import DashBoard from './DashBoard';
+import Header from './components/Header';
 describe('DashBorad', () => {
-  it('should render correctly in "debug" mode', () => {
-    const component = shallow(<DashBoard debug />);
-
-    expect(component).toMatchSnapshot();
+  it('should render has <Header/>', () => {
+    const wrapper = shallow(<DashBoard />);
+    expect(wrapper.find(Header).length).toBe(1);
   });
 });
---
+++ wd-tumblr/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = props => {
+  return <header>Header </header>;
+};
+
+export default Header;
