edc60dbcf9050cb9178bd563986052fada1ecd69
chany <lyhy0310@gmail.com>
2018-12-13 16:22:50 +0900

    delete (DashBoard test code):

--- wd-tumblr/src/DashBoard.js
+++ wd-tumblr/src/DashBoard.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
+
 const DashBoard = props => {
-  return (
-    <div>
-      <Header />
-      DashBoard
-    </div>
-  );
+  return <div>DashBoard</div>;
 };
 
 export default DashBoard;
--- wd-tumblr/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = props => {
-  return <header>Header </header>;
-};
-
-export default Header;
