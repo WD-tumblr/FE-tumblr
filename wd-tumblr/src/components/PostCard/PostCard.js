209515f6e75de7f3e8846a6554c4418016ec76fc
Log91 <peter1636@naver.com>
2018-12-20 17:09:42 +0900

    refactor(postCard): PostCard components를 functional에서 class 컴포넌트로 변경
    post card options를 click할 때 상태값으로 popover를 띄워주기 위해서 class형으로 수정

--- wd-tumblr/src/components/PostCard/PostCard.js
+++ wd-tumblr/src/components/PostCard/PostCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Avatar from '../Avatar';
 import Popover from '../Popover';
 import './PostCard.scss';
@@ -6,61 +6,64 @@ import './PostCard.scss';
 const handleOptionsButtonClicked = (e) => {
   e.preventDefault();
 };
-const PostCardList = (
-  {
-    userId, avatarImg, cardImg, cardImgAlt, title, content, tags,
-  },
-) => (
-  <div className="postCard__container">
-    <Avatar avatarImg={avatarImg} />
-    <div className="postCard">
-      <div className="postCard__header">
-        <span className="userId">{userId}</span>
-      </div>
-      <div className="postCard__content">
-        <div className="postCard__title-container">
-          <h2 className="postCard__heading">{title}</h2>
-        </div>
-        {cardImg && (
-          <div className="postCard__image-container">
-            <img src={cardImg} alt={cardImgAlt} />
+class PostCardList extends Component {
+  // {
+  //   userId, avatarImg, cardImg, cardImgAlt, title, content, tags,
+  // },
+  render() {
+    return (
+      <div className="postCard__container">
+        <Avatar />
+        <div className="postCard">
+          <div className="postCard__header">
+            <span className="userId">{this.props.userId}</span>
           </div>
-        )}
-        <div className="postCard__bodyText">
-          <p>{content}</p>
+          <div className="postCard__content">
+            <div className="postCard__title-container">
+              <h2 className="postCard__heading">{this.props.title}</h2>
+            </div>
+            {this.props.cardImg && (
+            <div className="postCard__image-container">
+              <img src={this.props.cardImg} alt={this.props.cardImgAlt} />
+            </div>
+            )}
+            <div className="postCard__bodyText">
+              <p>{this.props.content}</p>
+            </div>
+            {this.props.tags && (<div className="postCard__tags">{this.props.tags}</div>)}
+          </div>
+          <div className="postCard__footer">
+            <div className="notes__container" />
+            <ul className="postCard__buttons">
+              <li className="postCard__button">
+                <a href="">
+                  <i className="postCard__icon-share" />
+                </a>
+              </li>
+              <li className="postCard__button">
+                <a href="">
+                  <i className="postCard__icon-reply" />
+                </a>
+              </li>
+              <li className="postCard__button">
+                <a href="">
+                  <i className="postCard__icon-reblog" />
+                </a>
+              </li>
+              <li className="postCard__button">
+                <a href="">
+                  <i className="postCard__icon-options" onClick={handleOptionsButtonClicked}>
+                    <Popover />
+                  </i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        {tags && (<div className="postCard__tags">{tags}</div>)}
       </div>
-      <div className="postCard__footer">
-        <div className="notes__container" />
-        <ul className="postCard__buttons">
-          <li className="postCard__button">
-            <a href="">
-              <i className="postCard__icon-share" />
-            </a>
-          </li>
-          <li className="postCard__button">
-            <a href="">
-              <i className="postCard__icon-reply" />
-            </a>
-          </li>
-          <li className="postCard__button">
-            <a href="">
-              <i className="postCard__icon-reblog" />
-            </a>
-          </li>
-          <li className="postCard__button">
-            <a href="">
-              <i className="postCard__icon-options" onClick={handleOptionsButtonClicked}>
-                <Popover />
-              </i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 
 export default PostCardList;
